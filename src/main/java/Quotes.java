17a38fc19d3ee0d96212e41c13eac591204d83fa
bled1 <bllaca.bledi@axians.com>
2023-01-18 12:32:42 +0100

    init

---
+++ src/main/java/Quotes.java
@@ -0,0 +1,183 @@
+public class Quotes {
+  public static String quotes = "Buzëqeshja juaj është frymëzimi i vetëm që më duhet. Zëri juaj është motivi i vetëm për të cilin kam nevojë. Dashuria jote është e vetmja lumturi që më duhet. Mirëmëngjes." +
+      "/" +
+      "Duhet vetëm një sekondë për të menduar në mëngjes, por buzëqeshja në fytyrën time zgjat gjithë ditën. Mirëmëngjes." +
+      "/" +
+      "Dikush ju përqafon, por nuk jam unë - është mëngjesi ..." +
+      "Dikush ju ngroh, por nuk jam unë - është dielli ..." +
+      "Dikush ju mungon dhe ju do," +
+      "por ky nuk është as mëngjesi as dielli - ky jam unë." +
+      "/" +
+      "Po vij te ti me rrezet e para të diellit si një fllad i lehtë ere." +
+      "Për të të treguar sa më mungon dhe të të them mirëmëngjes dashuria ime." +
+      "/" +
+      "Më pëlqen të zgjohem pranë teje në mëngjes ... të shoh buzëqeshjen tënde të mrekullueshme, sytë madhështorë dhe të dëgjoj zërin tënd që thotë: \"mirëmëngjes i dashur\"." +
+      "/" +
+      "Unë e urrej zgjimin sepse më largon nga të gjitha ëndrrat që pashë me ty natën e kaluar. Mirëmëngjes." +
+      "/" +
+      "Jam lodhur që duhet të ëndërroj për ty çdo natë, të dua pranë meje që të shikoj pamjen tënde të bukur. Jam i lodhur duke u zgjuar çdo mëngjes dhe duke ju dërguar mesazhe, tani dua të kaloj mëngjeset e mia me ju. Me mungon, mirëmëngjes." +
+      "/" +
+      "Një nga gëzimet e jetës zgjohet çdo ditë me mendimin se diku dikush kujdeset aq sa të të dërgojë një përshëndetje të ngrohtë në mëngjes! Mirëmëngjes dhe shijojeni ditën!" +
+      "/" +
+      "Çdo ditë më bën të ndiej diçka që fjalët nuk mund ta shprehin kurrë. Mirëmëngjes." +
+      "/" +
+      "Unë nuk e di se sa kohë do të jemi së bashku sepse kjo nuk është e rëndësishme. Ajo që unë di dhe ajo që ka vërtet rëndësi është se nuk do të ndahemi kurrë. Mirëmëngjes." +
+      "/" +
+      "Kur mendojmë për problemet ato dyfishohen. por kur qeshim me ta, ato bëhen flluska. Lëreni shqetësimin dhe Mirëmëngjes!" +
+      "/" +
+      "Çdo mëngjes zgjohem për të vërtetën më të madhe të jetës sime - dashurinë time për ty. Mirëmëngjes." +
+      "/" +
+      "Ditë e re, bekim i ri. Mos lejoni që dështimet e djeshme të shkatërrojnë bukurinë e së sotmes, sepse çdo ditë ka premtimin e saj të dashurisë, gëzimit, faljes ..... Mirëmëngjes!" +
+      "/" +
+      "Të gjitha mëngjeset janë si piktura: Ju duhet pak frymëzim për të ecur përpara, skica e një buzëqeshjeje në fytyrën tuaj dhe një mesazh nga dikush që kujdeset të ngjyrosë ditën tuaj. Mirëmëngjes." +
+      "/" +
+      "Një buzëqeshje mund të hapë një zemër më shpejt sesa një çelës mund të hap një derë. Ajo ndriçon botën me buzëqeshjet tuaja. Mirëmëngjes!" +
+      "/" +
+      "Zoti na jep ëndrra natën, në mënyrë që t'i kthejmë ato në realitet gjatë ditës. Mëngjes të mirë dhe një ditë të suksesshme." +
+      "/" +
+      "Ekziston vetëm një ilaç për mëngjeset e ftohta - një përqafim i ngrohtë me ju. Mirëmëngjes." +
+      "/" +
+      "Nuk më intereson nëse dielli lind apo jo, mëngjesi im fillon vetëm pasi të them se të dua shumë. Mirëmëngjes." +
+      "/" +
+      "Gjithmonë zgjohu me një buzëqeshje dashuria ime, sepse je e fortë, e zgjuar, aktive dhe ke të gjitha bekimet." +
+      "/" +
+      "Fillimi i ditës time me ty më mbush me energji dhe bën që gjithçka të duket mirë. Mirëmëngjesi, princesha ime!" +
+      "/" +
+      "Mirëmëngjesi dashuria ime! Është e vështirë për mua të ngrihem dhe të largohem nga shtrati në mëngjes, sepse jam e detyruar të lë gruan e ëndrrave të mia." +
+      "/" +
+      "Zgjohuni dhe buzëqeshni, sepse kemi marrëdhënien më të bukur. Të dua!" +
+      "/" +
+      "Kur rrezet e diellit godasin fytyrën tonë në mëngjes, gjithçka që dua të ndiej janë puthjet e tua të ngrohta. Zgjohu, princesha ime!" +
+      "/" +
+      "Zgjohu dhe bëje botën të shkëlqejë! Të ëndërroj çdo natë dhe dua të të bëj gruan time." +
+      "/" +
+      "Një mëngjes i ëmbël për gruan që më bëri njeriun më të lumtur në botë." +
+      "/" +
+      "Zgjohuni dhe bëjeni botën të shkëlqejë! Është një ditë e mrekullueshme. Unë jam aq mirënjohës që ju jeni në jetën time. Unë dua që ju të dini se sa shumë ju dua." +
+      "/" +
+      "Mirëmëngjesi zemër! Shpresoj të keni fjetur si një mbretëreshë, ashtu siç ju përshtatet! Kalofsh nje ditë te mrekullueshme! Të dua shumë." +
+      "/" +
+      "Uroj që dita juaj të jetë plot gëzim dhe dashuri. Unë do të jem gjithmonë pranë teje kur të kesh nevojë për mua." +
+      "/" +
+      "Shumica e njerëzve ëndërrojnë të shkojnë në parajsë. Por unë parajsën e kam pranë dhe kjo parajsë je ti, dashuria ime! Mirëdita bukuroshja ime!" +
+      "/" +
+      "Mirëmëngjesi bukuroshe! Uroj të zgjohesh sot si një trëndafil i bukur dhe dita jote është e ëmbël si mjalti. Ju meritoni gjithçka, dashuria ime!" +
+      "/" +
+      "Unë dua që çdo mëngjes kur të zgjohem të të kem pranë meje në shtrat dhe të kem mundësi të shoh sytë e tu të bukur para se të të puth. Ky do të ishte një fillim i shkëlqyeshëm për ditën time. Mirëmëngjes zemër!" +
+      "/" +
+      "Mirëmëngjesi dashuria ime! Ju erdhët në jetën time kaq papritur nga askund, por tani ju jeni gjithçka për mua! Unë nuk mund të jetoj pa ty. Unë dua që ju të qëndroni me mua përgjithmonë. Unë të dua." +
+      "/" +
+      "Çdo ditë që kalon në jetën time e kuptoj që nuk është e zakonshme, sepse kam një grua unike në krah! Mirëmëngjes bukuria ime!" +
+      "/" +
+      "Unë ju dërgoj dashurinë time për të ndriçuar ditën tuaj, për t'ju bërë të ndiheni bukur dhe për t'ju kujtuar se dikush kujdeset shumë për ju." +
+      "/" +
+      "Asgjë nuk më jep më shumë gëzim sesa të të shoh të zgjohesh plot shëndet. Kam nevojë për buzëqeshjen tënde që të ndihem i lumtur dhe i gëzuar. Zgjohu dhe buzëqesh bukuria ime!" +
+      "/" +
+      "Edhe nëse nuk jam aty për të të zgjuar me puthje, shpresoj që me këtë mesazh të mund të të shpreh atë që kupton për mua. Nuk mund të ndaloj së dashuruari ty. Mirëmëngjes bukuria ime!" +
+      "/" +
+      "Mirëmëngjesi zemër! Unë jam personi më me fat në botë që të kam, sepse je thjesht unike. Uroj që dita juaj të jetë aq e bukur sa ju!" +
+      "/" +
+      "Uroj që dita juaj të jetë e mbushur me lumturi, si dita kur u takuam. Mirëmëngjes, dashuri e jetës time!" +
+      "/" +
+      "Unë nuk do të doja që kjo ditë të kalonte pa ju thënë sa e mahnitshme jeni dhe pa ju dërguar një mëngjes të mirë të ëmbël. Kalofsh nje ditë te mrekullueshme!" +
+      "/" +
+      "Me ty kam kuptuar se ajo që dua në jetën time është të zgjohem çdo mëngjes pranë teje dhe të shoh sytë e tu që janë më të ndritshëm se diamantet, të prek lëkurën tënde që është më verbuese se dielli dhe të ndjej prekjen tënde e cila është më e butë se mëndafshi. Unë të dua ty dhe asgjë tjetër në botë. Miremengjes! Ju uroj një ditë të mrekullueshme." +
+      "/" +
+      "Gjithçka është ende indiferente, sepse gruaja më e bukur në botë nuk është zgjuar ende. Zgjohuni dhe ndriçoni gjithçka rreth jush me bukurinë tuaj. Mirëmëngjes zemër!" +
+      "/" +
+      "Po të isha një muzikant, do të luaja një melodi të këndshme për ty çdo mëngjes dhe do të të këndoja këngët më të bukura që do të forconin admirimin tënd për mua. Ju jeni më shumë sesa e imagjinoja. Kalo një ditë të mahnitshme!" +
+      "/" +
+      "Dielli mund të shkëlqejë vetëm pasi të zgjoheni, sepse ju jeni agimi që ndriçon ditën time. Mirëmëngjesi Dashuria ime!" +
+      "/" +
+      "Buzëqesh! Sepse sapo keni marrë një mesazh me tekst nga dikush që ju intereson. Mirëmëngjes!" +
+      "/" +
+      "Çdo ditë më bën të ndiej diçka që fjalët nuk mund ta shprehin kurrë. Mirëmëngjes." +
+      "/" +
+      "Një buzëqeshje mund të hapë një zemër më shpejt sesa një çelës mund të hap një derë. Ajo ndriçon botën me buzëqeshjet tuaja. Mirëmëngjes!" +
+      "/" +
+      "Ekziston vetëm një ilaç për mëngjeset e ftohta - një përqafim i ngrohtë me ju. Mirëmëngjes." +
+      "/" +
+      "Një ditë nuk është asgjë e mirë pa diellin, pasi nata nuk është asgjë më e mirë pa hënën. Mëngjesi dhe mesdita ime nuk janë asgjë pa ty. Mirëmëngjes jeta ime dhe dashuria ime." +
+      "/" +
+      "Unë dua të jem arsyeja pse buzëqesh çdo mëngjes, dua të jem arsyeja e lumturisë në zemrën tënde, pranvera në hapin tënd dhe kënga në buzët e tua. Të keni një ditë të mrekullueshme!" +
+      "/" +
+      "Ndjenja më e mirë në botë është të dish që ti më takon mua dhe je e imja. Çdo mëngjes kjo është gjithçka që duhet të di dhe kjo është e mjaftueshme që unë të kaloj një ditë të mirë." +
+      "/" +
+      "Të gjitha shqetësimet e mia janë zhdukur, të gjitha betejat e mia kanë mbaruar! E ardhmja ime është e shkëlqyeshme me ty. Dhe tani që mund të parashikoj. Unë e dua jetën time me ty. Mirëmëngjes!" +
+      "/" +
+      "Shpresoj që mëngjesi juaj të jetë i ndritshëm si buzëqeshja juaj." +
+      "/" +
+      "Vetëm mendimi yt ma ndriçon mëngjesin." +
+      "/" +
+      "Nuk ka rëndësi nëse kam kafe apo çaj, asgjë nuk krahasohet me ndjenjën që më jepni. Miremengjes!" +
+      "/" +
+      "Ndonjëherë do të doja të mos kishte orë me zile, sepse kjo është pajisja e vetme që më zgjon kur ëndërroj për ty." +
+      "/" +
+      "Nga drita e parë e mëngjesit deri tek ylli i fundit i natës, kujto gjithmonë sa e veçantë je! Miremengjes!" +
+      "/" +
+      "Të gjitha mëngjeset janë si piktura: Ju duhet pak frymëzim për të ecur përpara, skica e një buzëqeshjeje në fytyrën tuaj dhe një mesazh nga dikush që kujdeset të ngjyrosë ditën tuaj. Miremengjes." +
+      "/" +
+      "Buzëqesh! Sepse sapo keni marrë një mesazh me tekst nga dikush që ju intereson. Miremengjes!" +
+      "/" +
+      "Çdo natë ëndërroj se çfarë do të doja të përqafoja ditën dhe çdo ditë e kaloja duke imagjinuar se si do të ishte të ëndërroja për ty natën. Miremengjes." +
+      "/" +
+      "Jeta është si një libër. Çdo ditë si një faqe e re. Kështu që fjalët e para që shkrove le të jenë Mirëmëngjesi dashuria ime!" +
+      "/" +
+      "Çdo ditë zgjohem dhe harroj të gjitha arsyet që më trishtojnë sepse më duhet vetëm një arsye për të më bërë të lumtur - JU. Miremengjes." +
+      "/" +
+      "Mirëmëngjesi! Nuk do të thotë se po ju kërkoj përgjigje. Është vetëm mënyra ime për të thënë \"ti je mendimi i parë i ditës time\"." +
+      "/" +
+      "Nuk ka rëndësi sa e vështirë mund të jetë jeta, ju jeni ishulli im i vogël i shpresës, forcës dhe mbështetjes në këtë oqean frikash dhe sfidash. Unë zgjohem çdo mëngjes për ty, dashuria ime. Duke ju dhënë shumë përqafime dhe puthje. Paçi një ditë të mbarë!" +
+      "/" +
+      "Të gjitha mëngjeset e mia nuk ishin asgjë të veçantë derisa u takuam. Që atëherë për mua është një nga periudhat më të bukura të ditës. Mirmengjesi dashuria ime ." +
+      "/" +
+      "Pa ty, nuk ka diell që të jetë rrezatues dhe nuk ka mëngjes që të jetë i bukur. Ti je kuptimi i jetës sime. Miremengjes!" +
+      "/" +
+      "Ti më bën të ndihem i veçantë çdo ditë dhe çdo mëngjes. Unë jam me fat që jem ai që ka një vend të veçantë në zemrën tuaj. Miremengjes!" +
+      "/" +
+      "Mirmengjesi zemer. Unë thjesht doja që ju të dini se dashuria ime është po aq e fortë sa dielli dhe do të shkëlqejë përgjithmonë ashtu si ai." +
+      "/" +
+      "Ti je gjëja e parë që më del në mendje sapo zgjohem. Pa dashurinë tënde, jeta ime është bosh dhe ditët e mia janë të mërzitshme! Miremengjes!" +
+      "/" +
+      "Një ditë e bukur po pret jashtë derës tuaj. Pra, zgjohu dhe bëhu pjesë e saj. Jeta juaj do të jetë plot dashuri në këtë ditë. Miremengjes!" +
+      "/" +
+      "Lumturia, telashet, suksesi, zhgënjimi apo dështimi – nuk e di se çfarë më pret ditën, por e di se gjithçka do të jetë mirë për sa kohë të kam ty në krahët e mi. Mëngjes i ëmbël e dashur!" +
+      "/" +
+      "Çdo mëngjes më kujton të gjitha ëndrrat e gabuara që kisha ndjekur gjatë gjithë jetës sime derisa gjeta të duhurën - Ty. Miremengjes." +
+      "/" +
+      "Buzëqeshja jote është e vetmja gjë që më ndriçon mëngjesin. Mezi pres të të shoh të më buzëqeshësh sot. Mirmengjesi vajzes se endrrave te mia." +
+      "/" +
+      "Mirëmëngjes, vajzë e bukur. E kalova natën duke ëndërruar për ty dhe dua ta kaloj ditën duke u përqafuar me ty." +
+      "/" +
+      "Çohu e dashura ime e fjetur, bëhu bukuroshe dhe shpërndaje dashurinë dhe hijeshinë përreth. Kalofsh nje ditë te mrekullueshme. Mirmengjesi bukuroshja ime." +
+      "/" +
+      "Çdo natë shkoj në shtrat duke uruar ëndrrat e ëmbla për ty, por kur zgjohem, më vjen mirë që dashuria jonë është realitet." +
+      "/" +
+      "Pavarësisht se sa mirë kam fjetur natën dhe çfarë ëndrrash të mrekullueshme kam parë, më pëlqen të zgjohem çdo mëngjes dhe të kuptoj se realiteti në të cilin jetoj është shumë më i mirë. Sepse ju jeni pjesa e këtij realiteti, pjesa më e mirë e tij. Mirëmëngjes, e dashur." +
+      "/" +
+      "Faleminderit që hyre në jetën time dhe e bëre atë të bukur. Emri yt është shkruar në zemrën time sepse ti meriton gjithçka në botë. Kalofshi një ditë të mrekullueshme." +
+      "/" +
+      "As një filxhan kafe nuk mund të zërë vendin tënd në jetën time. Nuk e di mënyrën e duhur për të shprehur ndjenjat e mia për ty, por e di që të dua me gjithçka që kam." +
+      "/" +
+      "Asnjë mëngjes nuk është i përsosur nëse nuk zgjohem pranë teje. Jam krenar që kam një vajzë të bukur që më është afër zemrës. Ju uroj mëngjesin më të mirë në botë." +
+      "/" +
+      "Ti je dielli që zgjon trupin tim në një ditë të re. Ti je melodia që luan në mendjen time kur ndihem i vetmuar. Je e vetmja vajzë në botë që më bën të ndihem i veçantë." +
+      "/" +
+      "Faleminderit që jeni në mendjen time këtë mëngjes dhe çdo ditë tjetër. Ti mbetesh burimi im i frymëzimit dhe nuk mund të ndalem së qenuri mirënjohës ndaj Zotit që të solli në jetën time." +
+      "/" +
+      "Si dielli, ti më ndriçon. Si era, ti më largon gjithë trishtimin. Si shiu, ti ma pastron shpirtin. Ashtu si toka, ti më mban kur jam gati të rrëzohem. Ti je bota për mua. Mirëmëngjes, e dashur! Nje dite e bukur per ju." +
+      "/" +
+      "Asgjë nuk krahasohet me ndjesinë që më jep buzëqeshja jote. Edhe yjet në qiell nuk i afrohen këtij krahasimi. Të dua më shumë çdo mëngjes." +
+      "/" +
+      "Pa ty, asnjë diell nuk është rrezatues dhe asnjë mëngjes nuk është i bukur. Ti je kuptimi i jetës sime. Miremengjes!" +
+      "/" +
+      "Arsyeja e lumturisë sime është dashuria dhe kujdesi juaj, dhe shpresoj që ju të ndiheni njësoj si unë. Uroj që këto ndjenja të mos zhduken kurrë, uroj që dashuria jonë të zgjasë përgjithmonë. Mirëmëngjesi Dashuria ime." +
+      "/" +
+      "Edhe pse jemi milje larg njëri-tjetrit, nuk jemi shumë larg njëri-tjetrit në zemër. Ndërsa zgjoheni në këtë mëngjes të mrekullueshëm, unë thjesht dua t'ju bëj të ditur se është dikush në anën tjetër të horizontit që me të vërtetë kujdeset aq shumë për ju. Mirëmëngjes dashuria ime dhe një ditë të mbarë!" +
+      "/" +
+      "Çdo mëngjes zgjohem me të vërtetën më të madhe të jetës sime – dashurinë time për ty. Miremengjes." +
+      "/" +
+      "E di, më mungon edhe kur fle. Të kam ëndërruar gjithë natën dhe mezi prisja të vinte agimi për të thënë sa shumë të dua, e dashur. Mirëmëngjes dhe fat të mirë!" +
+      "/" +
+      "Çdo mëngjes kur zgjohem, gjëja e parë që bëj është të shikoj ty për t'u siguruar që nuk je një ëndërr. Ende nuk mund ta besoj sa i bekuar jam që të kam në jetën time.";
+}
